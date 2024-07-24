4035b93dbc03a9d210610cc3bbf87cc4f61e590f
Савелий Крылов <savely.crylov@yandex.ru>
2024-07-24 12:42:49 +0300

    first commit

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\OrderStatus;
+use App\Enums\PaymentStatus;
+use App\Helpers\Cart;
+use App\Mail\NewOrderEmail;
+use App\Models\CartItem;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CheckoutController extends Controller
+{
+    public function checkout(Request $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
+        $customer = $user->customer;
+        if (!$customer->billingAddress || !$customer->shippingAddress) {
+            return redirect()->route('profile')->with('error', 'Please provide your address details first.');
+        }
+
+        [$products, $cartItems] = Cart::getProductsAndCartItems();
+
+        $orderItems = [];
+        $totalPrice = 0;
+
+        DB::beginTransaction();
+
+        foreach ($products as $product) {
+            $quantity = $cartItems[$product->id]['quantity'];
+            if ($product->quantity !== null && $product->quantity < $quantity) {
+                $message = match ($product->quantity) {
+                    0 => 'The product "'.$product->title.'" is out of stock',
+                    1 => 'There is only one item left for product "'.$product->title,
+                    default => 'There are only ' . $product->quantity . ' items left for product "'.$product->title,
+                };
+                return redirect()->back()->with('error', $message);
+            }
+        }
+
+        foreach ($products as $product) {
+            $quantity = $cartItems[$product->id]['quantity'];
+            $totalPrice += $product->price * $quantity;
+            $orderItems[] = [
+                'product_id' => $product->id,
+                'quantity' => $quantity,
+                'unit_price' => $product->price
+            ];
+
+            if ($product->quantity !== null) {
+                $product->quantity -= $quantity;
+                $product->save();
+            }
+        }
+
+        try {
+            // Create Order
+            $orderData = [
+                'total_price' => $totalPrice,
+                'status' => OrderStatus::Unpaid,
+                'created_by' => $user->id,
+                'updated_by' => $user->id,
+            ];
+            $order = Order::create($orderData);
+
+            // Create Order Items
+            foreach ($orderItems as $orderItem) {
+                $orderItem['order_id'] = $order->id;
+                OrderItem::create($orderItem);
+            }
+
+            // Create Payment
+            $paymentData = [
+                'order_id' => $order->id,
+                'amount' => $totalPrice,
+                'status' => PaymentStatus::Pending,
+                'type' => 'cc',
+                'created_by' => $user->id,
+                'updated_by' => $user->id,
+                'session_id' => 'fake-session-id'
+            ];
+            Payment::create($paymentData);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            Log::critical(__METHOD__ . ' method does not work. '. $e->getMessage());
+            throw $e;
+        }
+
+        DB::commit();
+        CartItem::where(['user_id' => $user->id])->delete();
+
+        return $this->fakeSuccessRedirect($order);
+    }
+
+    public function success(Request $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
+        try {
+            $session_id = 'fake-session-id'; // Используйте фиктивный session_id
+            $payment = Payment::query()
+                ->where(['session_id' => $session_id])
+                ->whereIn('status', [PaymentStatus::Pending, PaymentStatus::Paid])
+                ->first();
+            if (!$payment) {
+                throw new NotFoundHttpException();
+            }
+            if ($payment->status === PaymentStatus::Pending->value) {
+                $this->updateOrderAndSession($payment);
+            }
+
+            return view('checkout.success', compact('user'));
+        } catch (NotFoundHttpException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            return view('checkout.failure', ['message' => $e->getMessage()]);
+        }
+    }
+
+    public function failure(Request $request)
+    {
+        return view('checkout.failure', ['message' => ""]);
+    }
+
+    private function updateOrderAndSession(Payment $payment)
+    {
+        DB::beginTransaction();
+        try {
+            $payment->status = PaymentStatus::Paid->value;
+            $payment->update();
+
+            $order = $payment->order;
+
+            $order->status = OrderStatus::Unpaid;
+            $order->update();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::critical(__METHOD__ . ' method does not work. '. $e->getMessage());
+            throw $e;
+        }
+
+        DB::commit();
+
+        try {
+            $adminUsers = User::where('is_admin', 1)->get();
+
+            foreach ([...$adminUsers, $order->user] as $user) {
+                Mail::to($user)->send(new NewOrderEmail($order, (bool)$user->is_admin));
+            }
+        } catch (\Exception $e) {
+            Log::critical('Email sending does not work. '. $e->getMessage());
+        }
+    }
+
+    private function fakeSuccessRedirect(Order $order)
+    {
+        // Имитация успешного платежа
+        $session_id = 'fake-session-id';
+        $this->updateOrderAndSession($order->payment);
+
+        return redirect()->route('checkout.success', ['session_id' => $session_id]);
+    }
+}
